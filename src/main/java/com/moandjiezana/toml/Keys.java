8388e394ed790373ae78fb34b122e3bedef93762
moandji.ezana <mwanji@gmail.com>
2015-02-12 23:12:50 +0200

    Removed unused methods

--- src/main/java/com/moandjiezana/toml/Keys.java
+++ src/main/java/com/moandjiezana/toml/Keys.java
@@ -1,7 +1,5 @@
 package com.moandjiezana.toml;
 
-import static com.moandjiezana.toml.ValueConverters.isComment;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +22,6 @@ class Keys {
       }
     }
   }
-
 
   static Keys.Key[] split(String key) {
     List<Key> splitKey = new ArrayList<Key>();
@@ -67,105 +64,5 @@ class Keys {
     return splitKey.toArray(new Key[0]);
   }
 
-  static String getTableArrayName(String line) {
-    StringBuilder sb = new StringBuilder();
-    char[] chars = line.toCharArray();
-    boolean quoted = false;
-    boolean terminated = false;
-    int endIndex = -1;
-    boolean preKey = true;
-    
-    for (int i = 2; i < chars.length; i++) {
-      char c = chars[i];
-      if (c == '"' && chars[i - 1] != '\\') {
-        if (!quoted && i > 1 && chars [i - 1] != '.' && !Character.isWhitespace(chars[i - 1])) {
-          break;
-        }
-        quoted = !quoted;
-      } else if (!quoted && c == ']') {
-        if (chars.length > i + 1 && chars[i + 1] == ']') {
-          terminated = true;
-          endIndex = i + 1;
-          break;
-        }
-      } else if (!quoted && c == '.') {
-        preKey = true;
-      } else if (!quoted && Character.isWhitespace(c)) {
-        if (preKey && i > 2 && chars[i - 1] != '.' && !Character.isWhitespace(chars[i - 1])) {
-          break;
-        }
-        if (!preKey && chars.length > i + 1 && chars[i + 1] != '.' && chars[i + 1] != ']' && !Character.isWhitespace(chars[i + 1])) {
-          break;
-        }
-        continue;
-      } else if (!quoted && (ALLOWED_CHARS.indexOf(c) == -1)) {
-        break;
-      } else {
-        preKey = false;
-      }
-      
-      sb.append(c);
-    }
-    
-    String tableName = sb.toString();
-    
-    if (!terminated || tableName.isEmpty() || !isComment(line.substring(endIndex + 1))) {
-      return null;
-    }
-    
-    return StringConverter.STRING_PARSER.replaceUnicodeCharacters(tableName);
-  }
-  
-  /**
-   * @param line trimmed TOML line to parse
-   * @return null if line is not a valid table identifier
-   */
-  static String getTableName(String line) {
-    StringBuilder sb = new StringBuilder();
-    char[] chars = line.toCharArray();
-    boolean quoted = false;
-    boolean terminated = false;
-    int endIndex = -1;
-    boolean preKey = true;
-    
-    for (int i = 1; i < chars.length; i++) {
-      char c = chars[i];
-      if (c == '"' && chars[i - 1] != '\\') {
-        if (!quoted && i > 1 && chars [i - 1] != '.' && !Character.isWhitespace(chars[i - 1])) {
-          break;
-        }
-        quoted = !quoted;
-      } else if (!quoted && c == ']') {
-        terminated = true;
-        endIndex = i;
-        break;
-      } else if (!quoted && c == '.') {
-        preKey = true;
-      } else if (!quoted && Character.isWhitespace(c)) {
-        if (preKey && i > 1 && chars[i - 1] != '.' && !Character.isWhitespace(chars[i - 1])) {
-          break;
-        }
-        if (!preKey && chars.length > i + 1 && chars[i + 1] != '.' && chars[i + 1] != ']' && !Character.isWhitespace(chars[i + 1])) {
-          break;
-        }
-        continue;
-      } else if (!quoted && (ALLOWED_CHARS.indexOf(c) == -1)) {
-        break;
-      } else if (!quoted) {
-        preKey = false;
-      }
-      
-      sb.append(c);
-    }
-    
-    String tableName = sb.toString();
-    
-    if (!terminated || !isComment(line.substring(endIndex + 1))) {
-      return null;
-    }
-    
-    return StringConverter.STRING_PARSER.replaceUnicodeCharacters(tableName);
-  }
-  
   private Keys() {}
 }
