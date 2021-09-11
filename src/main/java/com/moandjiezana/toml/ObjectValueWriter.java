dd60f51c1b83f7701c31b05c93a0fb4197b8ae13
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2021-09-12 00:14:05 +0200

    Debugging illegal reflective access

--- src/main/java/com/moandjiezana/toml/ObjectValueWriter.java
+++ src/main/java/com/moandjiezana/toml/ObjectValueWriter.java
@@ -21,12 +21,49 @@ class ObjectValueWriter implements ValueWriter {
     write(value, context, null);
   }
 
+  private ObjectValueWriter() {
+  }
+
+  private static Set<Field> getFields(Class<?> cls) {
+    Set<Field> fields = new LinkedHashSet<>(Arrays.asList(cls.getDeclaredFields()));
+    while (cls != Object.class) {
+      fields.addAll(Arrays.asList(cls.getDeclaredFields()));
+      cls = cls.getSuperclass();
+    }
+    removeConstantsAndSyntheticFields(fields);
+
+    return fields;
+  }
+
+  private static void removeConstantsAndSyntheticFields(Set<Field> fields) {
+    fields.removeIf(field -> (Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) || field.isSynthetic() || Modifier.isTransient(field.getModifiers()) || field.isAnnotationPresent(TomlIgnore.class));
+  }
+
+  private static Object getFieldValue(Field field, Object o) {
+    //noinspection deprecation
+    boolean isAccessible = field.isAccessible();
+
+    if (!field.trySetAccessible()) return null;
+    Object value = null;
+    try {
+      value = field.get(o);
+    } catch (IllegalAccessException ignored) {
+    }
+    field.setAccessible(isAccessible);
+    return value;
+  }
+
+  @Override
+  public boolean isPrimitiveType() {
+    return false;
+  }
+
   public void write(Object value, WriterContext context, String[] objectComment) {
-    final Map<String, Object> to = new LinkedHashMap<String, Object>();
+    final Map<String, Object> to = new LinkedHashMap<>();
     final Set<Field> fields = getFields(value.getClass());
 
-    final ArrayList<String[]> comments = new ArrayList<String[]>();
-    final ArrayList<String[]> objComments = new ArrayList<String[]>();
+    final ArrayList<String[]> comments = new ArrayList<>();
+    final ArrayList<String[]> objComments = new ArrayList<>();
 
     for (Field field : fields) {
       final Object fieldValue = getFieldValue(field, value);
@@ -52,44 +89,4 @@ class ObjectValueWriter implements ValueWriter {
     }
     ((MapValueWriter) MAP_VALUE_WRITER).write(to, context, comments, objComments, objectComment);
   }
-
-  @Override
-  public boolean isPrimitiveType() {
-    return false;
-  }
-
-  private static Set<Field> getFields(Class<?> cls) {
-    Set<Field> fields = new LinkedHashSet<Field>(Arrays.asList(cls.getDeclaredFields()));
-    while (cls != Object.class) {
-      fields.addAll(Arrays.asList(cls.getDeclaredFields()));
-      cls = cls.getSuperclass();
-    }
-    removeConstantsAndSyntheticFields(fields);
-
-    return fields;
-  }
-
-  private static void removeConstantsAndSyntheticFields(Set<Field> fields) {
-    Iterator<Field> iterator = fields.iterator();
-    while (iterator.hasNext()) {
-      Field field = iterator.next();
-      if ((Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) || field.isSynthetic() || Modifier.isTransient(field.getModifiers()) || field.isAnnotationPresent(TomlIgnore.class)) {
-        iterator.remove();
-      }
-    }
-  }
-
-  private static Object getFieldValue(Field field, Object o) {
-    boolean isAccessible = field.isAccessible();
-    field.setAccessible(true);
-    Object value = null;
-    try {
-      value = field.get(o);
-    } catch (IllegalAccessException ignored) {
-    }
-    field.setAccessible(isAccessible);
-    return value;
-  }
-
-  private ObjectValueWriter() {}
 }
